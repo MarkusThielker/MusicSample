d257787f30a1142f5df8a9e98b8bf09fc646a434
Markus Thielker <mail.markus.thielker@gmail.com>
2021-04-25 15:58:11 +0200

    Add database requests to SongDao and SongRepository.

--- app/src/main/java/de/markus_thielker/uist_musicplayer/components/room/song/SongDao.kt
+++ app/src/main/java/de/markus_thielker/uist_musicplayer/components/room/song/SongDao.kt
@@ -1,10 +1,7 @@
 package de.markus_thielker.uist_musicplayer.components.room.song
 
 import androidx.lifecycle.LiveData
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import de.markus_thielker.uist_musicplayer.utilities.SONGS_TABLE_NAME
 
 @Dao
@@ -17,11 +14,14 @@ interface SongDao {
     fun findById(songId: Int): LiveData<List<Song>>
 
     @Query("SELECT * FROM $SONGS_TABLE_NAME WHERE title LIKE :title")
-    fun findByName(title: String): LiveData<List<Song>>
+    fun findByTitle(title: String): LiveData<List<Song>>
 
     @Query("SELECT * FROM $SONGS_TABLE_NAME WHERE artist LIKE :artist")
     fun findByArtist(artist: String): LiveData<List<Song>>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     fun insertAll(vararg songs: Song)
+
+    @Update
+    fun updateSong(song: Song)
 }
--- app/src/main/java/de/markus_thielker/uist_musicplayer/components/room/song/SongRepository.kt
+++ app/src/main/java/de/markus_thielker/uist_musicplayer/components/room/song/SongRepository.kt
@@ -8,4 +8,12 @@ class SongRepository(application: Application) {
     private val songDao = AppDatabase.getInstance(application).songDao()
 
     fun getAll() = songDao.getAll()
+
+    fun findById(songId: Int) = songDao.findById(songId)
+
+    fun findByTitle(title: String) = songDao.findByTitle(title)
+
+    fun findByArtist(artist: String) = songDao.findByArtist(artist)
+
+    fun updateSong(song: Song) = songDao.updateSong(song)
 }
