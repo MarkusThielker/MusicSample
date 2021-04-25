2d4dd83229c851db22f3f131c9117ae01362c3b9
Lukas Bormann <44904320+TheBormann@users.noreply.github.com>
2021-04-25 21:02:03 +0200

    feat: created the music player

--- app/src/main/java/de/markus_thielker/uist_musicplayer/MainActivity.kt
+++ app/src/main/java/de/markus_thielker/uist_musicplayer/MainActivity.kt
@@ -2,6 +2,7 @@ package de.markus_thielker.uist_musicplayer
 
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
@@ -23,6 +24,19 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+
+            if (destination.id == R.id.navigationFragmentPlayer) {
+                supportActionBar?.hide()
+                binding.floatingActionButton.hide()
+                binding.bottomNavigation.visibility = View.GONE
+            } else {
+                supportActionBar?.show()
+                binding.floatingActionButton.show()
+                binding.bottomNavigation.visibility = View.VISIBLE
+            }
+        }
+
         // connect action bar with navController
         val appBarConfiguration = AppBarConfiguration(
             setOf(
@@ -36,6 +50,13 @@ class MainActivity : AppCompatActivity() {
         // connect bottom navigation with navController
         val bottomNavigation = binding.bottomNavigation
         bottomNavigation.setupWithNavController(navController)
+
+        // Floating button
+        val fab = binding.floatingActionButton
+        fab.setOnClickListener { view ->
+            navController.navigate(R.id.action_global_navigationFragmentPlayer)
+        }
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/de/markus_thielker/uist_musicplayer/fragments/player/PlayerFragment.kt
+++ app/src/main/java/de/markus_thielker/uist_musicplayer/fragments/player/PlayerFragment.kt
@@ -7,6 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import de.markus_thielker.uist_musicplayer.R
 import de.markus_thielker.uist_musicplayer.databinding.FragmentPlayerBinding
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+
 
 class PlayerFragment : Fragment(R.layout.fragment_player) {
 
@@ -20,13 +23,44 @@ class PlayerFragment : Fragment(R.layout.fragment_player) {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        //(activity as AppCompatActivity?)!!.supportActionBar!!.hide()
         _binding = FragmentPlayerBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val backButton = binding.backButton
+        backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
+        val likeSongButton = binding.likeSongButton
+        likeSongButton.setOnClickListener {
+            //TODO add song to liked songs
+        }
+
+        val previousSong = binding.previousSong
+        previousSong.setOnClickListener {
+            //TODO play previous song
+        }
+
+        val nextSong = binding.nextSong
+        nextSong.setOnClickListener {
+            //TODO play next song
+        }
+
+        val stopMusic = binding.stopMusic
+        stopMusic.setOnClickListener {
+            //TODO stop the music
+        }
     }
 
     /** executed on fragment destroy */
     override fun onDestroyView() {
         super.onDestroyView()
+        //(activity as AppCompatActivity?)!!.supportActionBar!!.show()
         _binding = null
     }
 }
